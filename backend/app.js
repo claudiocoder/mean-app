7729b56b5b4abafdbb94c2baa706db96d89b5c54
Claudio Moreno <clauartur98@gmail.com>
2020-07-16 15:20:42 -0500

    feeat: add posts api books

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use((req,res,next)=>{
-    next();
+app.use((req, res, next) => {
+  next();
 });
 
-app.use((req,res,next)=>{
-    res.send('Hello from express');
+app.use("/api/posts", (req, res, next) => {
+  const post = [
+    { id: "qwtyuk11", title: "HarryPotter", content: "myPost" },
+    { id: "qwtyuk11", title: "HarryPotter", content: "myPost" },
+    { id: "qwtyuk11", title: "HarryPotter", content: "myPost" },
+  ];
+  return res.status(200).json({
+      message: 'Post fetched',
+      post: post
+  });
 });
 
 module.exports = app;
